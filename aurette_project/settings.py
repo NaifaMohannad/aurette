1a05740da4b6e26237166c6c9624aeff1c8c8a04
NaifaMohannad <naifamohannad@gmail.com>
2026-04-03 20:13:04 +0300

    Fix login URL settings

--- aurette_project/settings.py
+++ aurette_project/settings.py
@@ -120,3 +120,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
